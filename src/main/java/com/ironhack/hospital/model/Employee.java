d446c9cc80b184dfafd36e6f97f80199cce9c1c8
Blasphus666 <118141236+Blasphus666@users.noreply.github.com>
2024-06-01 14:39:45 +0200

    Rebrand

---
+++ src/main/java/com/ironhack/hospital/model/Employee.java
@@ -0,0 +1,50 @@
+package com.ironhack.hospital.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+    private String department;
+    private String name;
+    private String status;
+
+
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/repository/PatientRepository.java
@@ -0,0 +1,13 @@
+package com.ironhack.hospital.repository;
+
+import com.ironhack.hospital.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findByDateOfBirthBetween(LocalDate startDate, LocalDate endDate);
+    List<Patient> findByAdmittedByDepartment(String department);
+    List<Patient> findByAdmittedByStatus(String status);
+}
